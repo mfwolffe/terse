0c4e16a7f6c705248a7ce2623969818b6b89bd49
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-12-14 00:37:29 -0500

    disable minutes decrement past zero

--- src/app/components/ComplaintForm.tsx
+++ src/app/components/ComplaintForm.tsx
@@ -99,6 +99,12 @@ const ComplaintRadio = ({ minutes=-1, setMinutes }) => {
     if (!e) return;
 
     const value = e.target.value;
+
+    console.log(`val ${value}`);
+    
+
+    if (value < 0) return;
+
     setMinutes(value);
   }
 
@@ -107,10 +113,11 @@ const ComplaintRadio = ({ minutes=-1, setMinutes }) => {
   }, [minutes])
 
 return (
+      // TODO matt fix this lol 
       <div className="w-100 ml-auto mr-auto text-center">
         <label htmlFor="bedrooms-input" className="block mb-2 text-sm font-medium">Choose quantity:</label>
         <div className="relative flex items-center max-w-[11rem] ml-auto mr-auto">
-            <button onClick={() => setMinutes(+minutes - 1)} type="button" id="decrement-button" data-input-counter-decrement="bedrooms-input" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+            <button disabled={+minutes == 0} onClick={() => setMinutes(+minutes - 1)} type="button" id="decrement-button" data-input-counter-decrement="bedrooms-input" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                 <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16"/>
                 </svg>
