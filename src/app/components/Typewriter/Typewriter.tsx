61ae4599d96c03bdc1079986bc8f5379a455a738
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-12-24 18:37:02 -0500

    make typewriter anim feel more natural

--- src/app/components/Typewriter/Typewriter.tsx
+++ src/app/components/Typewriter/Typewriter.tsx
@@ -1,7 +1,7 @@
+"use client";
+
 import React, { useEffect, useState } from 'react';
 import styles from './Typewriter.module.css';
-import { randIntLeftInclusiveRange as randIntExcl } from '@/app/utils/common';
-import { randIntFromInclusiveRange as randIntIncl } from '@/app/utils/common';
 
 const Typewriter: React.FC = () => {
   const [text, setText] = useState('');                 // the string to render w/ typewriter effect
@@ -13,44 +13,78 @@ const Typewriter: React.FC = () => {
   const final = 'tldr?';                      // final string to render
   const intrmdry = 'too long, didn\'t read?'; // intermediary string (is typed out first, then deleted)
 
+  const deleteSpeed           = 1;
+  const baseSpeed             = 22;
+  const terserSpeed           = 12;
+  const commaPauseSpeed       = 225;
+  const questionPauseSpeed    = 135;
+  const apostrophePauseSpeed  = 165;
+
   // thought about reversing the strings to simplify 
   // const dupeFinal = [...final].reverse().join("");
   // const dupeIntrmdry = [...intrmdry].reverse().join("");
 
-  const PAUSESPEED = 700;
-
   useEffect(() => {
     const typewriterEffect = () => {
       let timer: NodeJS.Timeout;
-      let basespeed = 24;
-      let speed = basespeed;
-
+      
+      let chkIndx = text.length;
+      
       if (isTyping) {
         if (text === intrmdry) {
-          setIsTyping(false);
-          setIsDeleting(true);
-        } else {
-          if (intrmdry[text.length] === '?')        speed = PAUSESPEED;
-          else if (intrmdry[text.length] === ',')   speed = basespeed * randIntIncl(8, 12);
-          else if (intrmdry[text.length] === '\'')  speed = basespeed * randIntIncl(4, 10);
-          else speed = basespeed;
-
-          timer = setTimeout(() => { setText(intrmdry.slice(0, text.length + 1)); }, speed);
+          setTimeout(() => {
+            setIsTyping(false);
+            setIsDeleting(true);
+          }, 1400);
         }
+        else if (chkIndx > -1 && intrmdry.substring(chkIndx - 2, chkIndx) === 'g,') {          
+          setIsTyping(false);
+          setTimeout(() => {
+            timer = setTimeout(() => { setText(intrmdry.slice(0, chkIndx + 1)); }, baseSpeed);
+            setIsTyping(true);
+          }, commaPauseSpeed);
+        }
+        else if (chkIndx < intrmdry.length && intrmdry[chkIndx] === '\'') {
+          setIsTyping(false);
+          setTimeout(() => {
+            timer = setTimeout(() => { setText(intrmdry.slice(0, chkIndx + 1)); }, baseSpeed);
+            setIsTyping(true);
+          }, apostrophePauseSpeed);
+        }
+        else if (chkIndx < intrmdry.length && intrmdry[chkIndx] === '?') {
+          setIsTyping(false);
+          setTimeout(() => {
+            timer = setTimeout(() => { setText(intrmdry.slice(0, chkIndx + 1)); }, baseSpeed);
+            setIsTyping(true);
+          }, questionPauseSpeed);
+        }
+        else
+          timer = setTimeout(() => { setText(intrmdry.slice(0, chkIndx + 1)); }, baseSpeed);
       }
-      
+
       else if (isDeleting) {
         if (text === '') {
           setIsDeleting(false);
           setIsTersing(true);
-        } else timer = setTimeout(() => { setText(text.slice(0, -1)); }, basespeed / 2)  
+        }
+        else
+          timer = setTimeout(() => { setText(text.slice(0, -1)); }, deleteSpeed)  
       }
 
       else if (isTersing) {
         if (text === final) {
           setIsTersing(false);
           setIsTerse(true);
-        } else timer = setTimeout(() => { setText(final.slice(0, text.length + 1)); }, basespeed * 3/4);  
+        }
+        if (chkIndx < final.length && final[chkIndx] === '?') {
+          setIsTersing(false);
+          setTimeout(() => {
+            timer = setTimeout(() => { setText(final.slice(0, text.length + 1)); }, terserSpeed);
+            setIsTersing(true);
+          }, questionPauseSpeed * 2)
+        }
+        else
+          timer = setTimeout(() => { setText(final.slice(0, text.length + 1)); }, terserSpeed);
       }
 
       return () => clearTimeout(timer);
