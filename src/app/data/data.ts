ece9fb0c879d234a2e82fe1cea94b71a230b5a58
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-12-24 18:37:25 -0500

    move globals to static location

---
+++ src/app/data/data.ts
@@ -0,0 +1,63 @@
+// data for terse
+// atm this site is built statically, and is fairly small
+// so I'm just dropping things like this in a data file
+
+
+const affiliations = [
+  " JMU ",
+  " UNC ",
+  " MHS ",
+  " BRCC ",
+  " HPCI ",
+];
+
+
+const technologies = [
+  " muon cloud disruptors",
+  " tachyonic quark fusers",
+  " dark matter zeta beams",
+  " dark energy stabilizers",
+  " neutron flux regulators",
+  " chaotic boson entanglers",
+  " muon dispersion matrices",
+  " neutrino-flux capacitors",
+  " harmonic quark regulators",
+  " protonic whim oscillators",
+  " photon disruption matrices",
+  " photonic graviton enhancers",
+  " plasma-infused charm particles",
+  " antimatter harmonic resonators",
+  " phase-shifted neutrino vortices",
+  " polychromatic particle emitters",
+  " photon-inverted tachyon matrices",
+  " polymorphic graviton stabilizers",
+];
+
+
+const sources = [
+  " channel the power of galactic quirk ",
+  " channel the energy of void harmonics ",
+  " harness the symphony of neutrino flux ",
+  " leverage the flux of galactic cadences ",
+  " harness the waves of astral oscillation ",
+  " leverage the cadence of stellar vortices ",
+  " exploit the murmur of cosmic divergences ",
+  " exploit the pulsations of cosmic rhythms ",
+  " channel the resonance of subatomic tumult ",
+  " employ the vibrations of astral harmonics ",
+  " tap into the vortex of starlight harmonics ",
+  " channel the murmurs of interstellar whispers ",
+  " tap into the quintessence of baryonic eddies ",
+  " utilize the currents of cosmic reverberation ",
+  " employ the oscillations of temporal substrates ",
+  " tap into the essence of the cosmic possum posse ",
+  " utilize the reverberations of subatomic symphonies ",
+  " utilize the ethereal essence of aetherial harmonics ",
+];
+
+
+export {
+  sources,
+  affiliations,
+  technologies,
+};
