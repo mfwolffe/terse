9952fc6b06b73a7f3ae257377d675b22f6d8b009
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-06 15:02:56 -0500

    resolve empty form breaks formspring

--- src/app/components/ComplaintForm.tsx
+++ src/app/components/ComplaintForm.tsx
@@ -145,7 +145,7 @@ export default function ComplaintForm() {
       { mode === 5 && <ComplaintSelect minutes={minutes} setMinutes={setMinutes} /> }
       { mode === 4 && <InsultViewer insult={insult} setInsult={setInsult} /> }
 
-
+      <input type="hidden" name="log-only?" value={JSON.stringify(mode === 1)} />
       <div className="ml-auto mr-auto w-100 flex justify-end">
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Submit
