f70283b50000f242e739b31ea86ec14b6fdc7fa9
mfwolffe <wolffemf@dukes.jmu.edu>
2025-01-06 16:19:51 -0500

    force validation on adding new suggest

--- src/app/components/UserFeedbackTable/UserFeedbackTable.tsx
+++ src/app/components/UserFeedbackTable/UserFeedbackTable.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import { Suggestion } from "@/types";
-
 import { List } from "flowbite-react";
 import { useCallback, useState } from "react";
 import { Label, Textarea } from "flowbite-react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlus } from "@awesome.me/kit-361830ecc8/icons/duotone/light";
-
 
 const addNew = <FontAwesomeIcon icon={faCirclePlus} fontSize={"2rem"} className="" />;
 
@@ -15,13 +13,24 @@ const SuggestionRow: React.FC<{ suggestion: Suggestion; onChange: (id: number, n
   suggestion,
   onChange,
 }) => {
+  const handleTextChange = (e: any) => {
+    onChange(suggestion.id, e.target.name, e.target.value);
+  };
+
   return (
     <List.Item className="flex w-full gap-8 mb-4">
       <div className="basis-1/2">
         <div className="mb-2 block">
           <Label htmlFor={`mattText-${suggestion.id}`} value="What matt wrote:" className="text-[var(--form-invalid)] font-medium" />
         </div>
-        <Textarea id={`mattText-${suggestion.id}`} name={`mattText-${suggestion.id}`} className="w-[98%]  ml-auto mr-auto" required />
+        <Textarea
+          id={`mattText-${suggestion.id}`}
+          name="mattText"
+          value={suggestion.mattText}
+          onChange={handleTextChange}
+          className="w-[98%]  ml-auto mr-auto"
+          required
+        />
       </div>
 
       <div className="basis-1/2">
@@ -32,33 +41,51 @@ const SuggestionRow: React.FC<{ suggestion: Suggestion; onChange: (id: number, n
             className="text-[var(--form-valid-muted)] font-medium"
           />
         </div>
-        <Textarea id={`userText-${suggestion.id}`} name={`userText-${suggestion.id}`} className="w-[98%] ml-auto mr-auto" required />
+        <Textarea
+          id={`userText-${suggestion.id}`}
+          name="userText"
+          value={suggestion.userText}
+          onChange={handleTextChange}
+          className="w-[98%] ml-auto mr-auto"
+          required
+        />
       </div>
     </List.Item>
   );
 };
 
 function SuggestionList() {
-  const [suggestions, setSuggestions] = useState<Suggestion[]>([{ id: 1, mattText: "", userText: "" }]);
+  const [suggestions, setSuggestions] = useState<Suggestion[]>([{ id: 0, mattText: "", userText: "" }]);
 
   const handleAddNew = useCallback(() => {
-    setSuggestions((prev) => [...prev, { id: prev.length + 1, mattText: "", userText: "" }]);
-  }, []);
+    const lastSuggestion = suggestions[suggestions.length - 1];
+
+    if (!lastSuggestion.mattText.trim() || !lastSuggestion.userText.trim()) {
+      alert("Please fill in all text areas before adding a new row.");
+      return;
+    }
+
+    setSuggestions((prev) => [...prev, { id: prev.length, mattText: "", userText: "" }]);
+  }, [suggestions]);
 
   const handleChange = useCallback((id: number, name: string, value: string) => {
-    setSuggestions((prev) => prev.map((suggestion) => (suggestion.id === id ? { ...suggestion, [name]: value } : suggestion)));
+    setSuggestions((prev) =>
+      prev.map((suggestion) => (suggestion.id === id ? { ...suggestion, [name]: value } : suggestion))
+    );
   }, []);
 
   return (
     <List unstyled className="mb-4">
-
-      { suggestions.map((suggestion) => ( <SuggestionRow key={suggestion.id} suggestion={suggestion} onChange={handleChange} /> )) }
+      {suggestions.map((suggestion) => (
+        <SuggestionRow key={suggestion.id} suggestion={suggestion} onChange={handleChange} />
+      ))}
 
       <List.Item className="flex items-center">
         <button
           type="button"
           className="ml-auto text-[var(--p2cls)] bg-transparent hover:text-white focus:ring-inset focus:outline-none  rounded-full text-sm p-2.5 text-center inline-flex items-center dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500"
-          onClick={handleAddNew}>
+          onClick={handleAddNew}
+        >
           {addNew}
           <span className="sr-only">add new suggestion icon</span>
         </button>
